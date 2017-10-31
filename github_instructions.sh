b3cd1750ab91072d66d8e2f68c8229f1edaf33b0
William <williamsoftwarecode@gmail.com>
2017-10-31 05:49:41 +0000

    Commit Message

---
+++ github_instructions.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+git init
+git add .
+git commit -m "Commit Message"
+git remote set-url https://github.com/williamsoftwarecode/python.git
+git pull origin master --allow-unrelated-histories
+git push origin master
